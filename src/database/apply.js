499dbb91ba0e5d5795b6b4a8ab008d46b1022475
James-Wong <380928526@qq.com>
2018-07-11 23:54:12 +0800

    add

--- src/database/apply.js
+++ src/database/apply.js
@@ -5,6 +5,16 @@
 // 户数（户） households
 // 状态 state  正常/锁止
 
+// 往里面添加数据，下面是基础结构
+// {
+//     name: '天河区车陂旧村庄全面改造项目',
+//     process: '项目审阅',
+//     number: '20181002-cb',
+//     address: '广州市天河区车陂',
+//     households: '100',
+//     state: '正常'
+// }
+
 let applyTable = [{
     name: '天河区车陂旧村庄全面改造项目',
     process: '项目申请',
